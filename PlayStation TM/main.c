0b8386dd8e0887e881f58b1e3f2fbfb4349da45c
TotoShampoin <thomasgarnier2010@hotmail.fr>
2019-12-01 18:58:28 +0100

    PLAYSTATION

---
+++ PlayStation TM/main.c
@@ -0,0 +1,21 @@
+#include <lib.h>
+
+
+//Fonctions externes
+void AdvancedPrint(int,int,char*);
+void APlot(int,int,char,int);
+short rand124();
+short testmem(void *,short);
+void delai(char );
+void CG_DEFCHAR();
+void setanimate(int,int);
+void unsetanimate();
+
+//Variables
+
+
+//Main
+void main()
+{
+	
+}
